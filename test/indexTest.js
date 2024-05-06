fcbc0f7a64fbdeb9f7e8fb831a78f8a4707195b7
Muhammad S. Ibrahim <hama.s.ibrahim9@gmail.com>
2024-05-06 18:42:20 +0300

    Done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-require ( './root.js' );
+require("./root.js");
 
 const { name, height, message } = require("../index.js");
 
@@ -26,13 +26,26 @@ describe('what-is-a-test', () => {
 })
 */
 
-// The code below ensures that students who are using CodeGrade will get credit 
+// The code below ensures that students who are using CodeGrade will get credit
 // for the code-along in Canvas; you can disregard it.
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
-      return true;
-    })
-  })
-})
+describe("what-is-a-test", () => {
+  describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+
+  describe("Height", () => {
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
+});
